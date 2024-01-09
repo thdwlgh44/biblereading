8016a01b95a82e4b8c126413de9c8f2f5e9baa1e
jihoSong <thdwlgh44@naver.com>
2024-01-09 09:40:48 +0900

    24.01.09 환경세팅

---
+++ src/main/java/com/bible/biblereading/BiblereadingApplication.java
@@ -0,0 +1,13 @@
+package com.bible.biblereading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblereadingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiblereadingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bible/biblereading/BiblereadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bible.biblereading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblereadingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
